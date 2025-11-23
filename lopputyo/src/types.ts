c469c50ff8084fa56396aad257a9488b0b0ebb26
nina negahdar <bhm577@myy.haaga-helia.fi>
2025-11-23 17:52:30 +0200

    restart

--- lopputyo/src/types.ts
+++ lopputyo/src/types.ts
@@ -1,20 +1,14 @@
 export type Customer = {
-    firstname : string;
-    lastname : string;
-    streetaddress : string;
-    postcode : string;
-    city : string;
-    email : string;
-    phone : string;
-    _links: {
-        self: {
-            href: string;
-        },
-        customer: {
-            href: string;
-        },
-        trainings : {
-            href: string;
-        }
-    }
-}
+  firstname: string;
+  lastname: string;
+  streetaddress: string;
+  postcode: string;
+  city: string;
+  email: string;
+  phone: string;
+  _links: {
+    self: { href: string };
+    customer: { href: string };
+    trainings: { href: string };
+  };
+};
