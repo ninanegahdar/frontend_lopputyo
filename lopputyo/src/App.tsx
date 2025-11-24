bfbff72dff9e0693489ce05da74ca05a4964eede
nina negahdar <bhm577@myy.haaga-helia.fi>
2025-11-24 18:12:28 +0200

    feat: Appbar, Typography, CssBaseline lisätty App.tsx

--- lopputyo/src/App.tsx
+++ lopputyo/src/App.tsx
@@ -1,11 +1,26 @@
 import CustomerList from "./components/CustomerList"
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 function App() {
-    return (
+  return (
     <>
-    <CustomerList />
+    <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Customers
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="lg" sx={{ mt: 4 }}>
+        <CustomerList />
+      </Container>
     </>
-    )
+  );
 }
 
-export default App
+export default App;
