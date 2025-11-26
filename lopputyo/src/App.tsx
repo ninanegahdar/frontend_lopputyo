c8a9727eeeaa5a372f6fe18578f908144617e1d5
nina negahdar <bhm577@myy.haaga-helia.fi>
2025-11-26 02:42:23 +0200

    revert: undid previous commit for main.tsx and App.tsx

--- lopputyo/src/App.tsx
+++ lopputyo/src/App.tsx
@@ -1,11 +1,10 @@
-import CustomerList from "./components/CustomerList";
-import TrainingList from "./components/TrainingList";
+import CustomerList from "./components/CustomerList"
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { Link, Outlet } from 'react-router';
+import TrainingList from "./components/TrainingList";
 
 function App() {
   return (
@@ -14,19 +13,13 @@ function App() {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            Personal Training
+            Customers
           </Typography>
-
-            <nav>
-              <Link to={"/"}>Home</Link>
-              <Link to={"/customers"}>Customers</Link>
-              <Link to={"/trainings"}>Trainings</Link>
-            </nav>
         </Toolbar>
       </AppBar>
-
       <Container maxWidth="lg" sx={{ mt: 4 }}>
-        <Outlet />{/*Home/Customers/Trainings*/}
+        <CustomerList />
+        <TrainingList/>
       </Container>
     </>
   );
