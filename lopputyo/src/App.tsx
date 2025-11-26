457d36bdbfadaa6bf3f59e285e1a27c028644df0
nina negahdar <bhm577@myy.haaga-helia.fi>
2025-11-26 17:49:27 +0200

    feat: navigation added to App.tsx

--- lopputyo/src/App.tsx
+++ lopputyo/src/App.tsx
@@ -1,27 +1,38 @@
-import CustomerList from "./components/CustomerList"
+import CustomerList from "./components/CustomerList";
+import TrainingList from "./components/TrainingList";
+import Home from "./Home";
+
+import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import TrainingList from "./components/TrainingList";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
     <CssBaseline />
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            Customers
+            Personal Training
           </Typography>
+          {/* Navigation */}
+            <Button color="inherit" component={Link} to="/">Home</Button>
+            <Button color="inherit" component={Link} to="/customers">Customers</Button>
+            <Button color="inherit" component={Link} to="/trainings">Trainings</Button>
         </Toolbar>
       </AppBar>
       <Container maxWidth="lg" sx={{ mt: 4 }}>
-        <CustomerList />
-        <TrainingList/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/customers" element={<CustomerList />} />
+          <Route path="/trainings" element={<TrainingList />} />
+        </Routes>
       </Container>
-    </>
+    </Router>
   );
 }
 
--- lopputyo/src/Home.tsx
+++ lopputyo/src/Home.tsx
@@ -1,7 +1,8 @@
-
-export default function Home() {
+function Home() {
 
     return (
     <h3>Homepage</h3>
     )
 }
+
+export default Home;
