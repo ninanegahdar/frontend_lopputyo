73a8e024b8b139c02f91a7e16c1ec425da0f2ca0
nina negahdar <bhm577@myy.haaga-helia.fi>
2025-12-03 15:57:12 +0200

    style: mui card component with welcome text added to homepage.

--- lopputyo/src/Home.tsx
+++ lopputyo/src/Home.tsx
@@ -1,8 +1,21 @@
-function Home() {
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
-    return (
-    <h3>Homepage</h3>
-    )
+
+    function Home() {
+        return (
+        <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+            <Typography gutterBottom sx={{ fontSize: 25 }}>
+            Welcome to the Personal Trainer Dashboard.
+            </Typography>
+            <Typography>
+                Manage customers, track trainings & keep everything organized in one place.
+            </Typography>
+        </CardContent>
+    </Card>
+    );
 }
 
 export default Home;
